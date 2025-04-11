5a2ecbe922693769e1e4a3a68a751f6460dea391
mandujano_cruz <jesus.mandujano.cruz@outlook.com>
2025-04-11 04:57:13 -0600

    fix: se agrega overlay en popup

--- src/components/Main/components/Popup/Popup.jsx
+++ src/components/Main/components/Popup/Popup.jsx
@@ -1,10 +1,18 @@
 export default function Popup(props) {
   const {onClose, title, children, classPopup} = props;
+
+  function handleOverlayClick(event) {
+    if (event.target.classList.contains("overlay")) {
+      onClose();
+    }
+  }
   return (
-    <div className={`popup ${classPopup}`}>
-      <button className="popup__close" onClick={onClose}></button>
-      {title && <h2 className="popup__title">{title}</h2>}
-      {children}
+    <div className="overlay" onClick={handleOverlayClick}>
+      <div className={`popup ${classPopup}`}>
+        <button className="popup__close" onClick={onClose}></button>
+        {title && <h2 className="popup__title">{title}</h2>}
+        {children}
+      </div>  
     </div>
   )
 }
